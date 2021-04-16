12a1d8a7ddebf19f40669fd45958758e20c81514
Jong9106 <juank_hr@hotmail.com>
2021-04-16 00:17:14 -0500

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,17 +1,109 @@
 #!/usr/bin/python3
-""" Module for testing Console"""
+"""
+Module for test the console
+"""
+from models.base_model import BaseModel
 import unittest
-import pep8
+import datetime
+from uuid import UUID
+import json
+import os
 
 
-class TestConsole(unittest.TestCase):
-    """ Class to test the console method """
+class test_basemodel(unittest.TestCase):
+    """ """
 
-    def test_Pep8(self):
-        """
-        test_pep8
-        test for pep8
-        """
-        pep8val = pep8.StyleGuide(quiet=True)
-        pep8checks = pep8val.check_files(["console.py"])
-        self.assertEqual(pep8checks.total_errors, 0, "pep8 fail")
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
+
+    def setUp(self):
+        """ """
+        pass
+
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_default(self):
+        """ """
+        i = self.value()
+        self.assertEqual(type(i), self.value)
+
+    def test_kwargs(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
+
+    def test_kwargs_int(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
+
+    def test_save(self):
+        """ Testing save """
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
+
+    # @unittest.skip("This test is not working well")
+    def test_str(self):
+        """ """
+        i = self.value()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
+                         i.__dict__))
+
+    def test_todict(self):
+        """ """
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
+
+    def test_kwargs_none(self):
+        """ """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+
+    @unittest.skip("Now the program accept kwargs")
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        with self.assertRaises(KeyError):
+            new = self.value(**n)
+
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        new = self.value(**n)
+        self.assertEqual(new.Name, 'test')
+
+    def test_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = BaseModel(**n)
+        self.assertFalse(new.created_at == new.updated_at)
